bf2de6925b5e9b30949b7f7c226b32b6e72039c8
sawroar <sawroarhossain5@gmail.com>
2022-09-28 22:12:15 +0600

    header section completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from '../../multifitness.png'
+import logo from '../../multi.svg'
 const Header = () => {
     return (
-        <div className='d-flex align-items-center container  text-center '>
-            <img src={logo} className='img-fluid' height="100px" width="300px" alt="" />
-            <h1 className='text-primary'>Multi Fitness Center</h1>
+        <div className='' >
+            <div className='d-flex align-items-center text-center'>
+                <img src={logo} className='img-fluid' height="100px" width="200px" alt="" />
+                <h1 className='text-primary'>Multi Fitness Center</h1>
+            </div>
+            <h3 className='ms-4'>Select Your Exercise</h3>
         </div>
     );
 };
