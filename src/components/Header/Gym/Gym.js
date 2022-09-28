ee0dabfe1d5d592857c242793fef54426b3f64b2
sawroar <sawroarhossain5@gmail.com>
2022-09-29 03:10:44 +0600

    responsive done

--- src/components/Header/Gym/Gym.js
+++ src/components/Header/Gym/Gym.js
@@ -25,15 +25,17 @@ const Gym = () => {
 
 
     return (
-        <div className='row'>
-            <div className='col-8  border '>
-                <div className='row'>
-                    <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5'> {exercises.map(exercise => <Exercise key={exercise.id} exercise={exercise} handleClick={handleClick}></Exercise>)}
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-8 col-md-8 col-sm-12 border '>
+                    <div className='row'>
+                        <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5'> {exercises.map(exercise => <Exercise key={exercise.id} exercise={exercise} handleClick={handleClick}></Exercise>)}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='col-4 border'>
-                <Exercisedetails cart={cart} btnClick={btnClick}></Exercisedetails>
+                <div className='col-lg-4 col-md-4 col-sm-12 border'>
+                    <Exercisedetails cart={cart} btnClick={btnClick}></Exercisedetails>
+                </div>
             </div>
 
         </div>
