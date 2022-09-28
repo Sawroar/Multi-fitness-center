b41e75cf03a1d75658d40de68742a3fd22d9ff8b
sawroar <sawroarhossain5@gmail.com>
2022-09-28 23:47:04 +0600

    card section done

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+
 const Exercise = (props) => {
-    const { name, img, info, age, time } = props.exercise
+    const { name, image, info, age, time } = props.exercise
     return (
-        <div>
-            <img src={img} alt="" />
-        </div>
+        <div className='exercise'>
+
+            <div className=''>
+                <div className="col-">
+                    <div className="card h-100">
+                        <img src={image} class="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{name}</h5>
+                            <p className="card-text">{info}</p>
+                            <h5>For Age:{age}</h5>
+                            <h5>Time Required:{time}</h5>
+                            <button className='btn btn-primary w-100'>Add to list</button>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+            {/* <img src={img} className='img-fluid' alt="" />
+            <h4>{name}</h4>
+            <p>{info}</p>
+            <h5>For Age:{age}</h5>
+            <h5>Time Required:{time}</h5> */}
+        </div >
     );
 };
 
--- src/components/Header/Gym/Gym.js
+++ src/components/Header/Gym/Gym.js
@@ -12,9 +12,12 @@ const Gym = () => {
     }, []);
     return (
         <div className='row'>
-            <div className='col-8 border'>
-                {exercises.map(exercise => <Exercise key={exercise.id} exercise={exercise}></Exercise>)}</div>
-            <div className='col-4 border'> <h1>how are you </h1></div>
+            <div className='col-10  border '><div className='row'>
+
+                <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5'> {exercises.map(exercise => <Exercise key={exercise.id} exercise={exercise}></Exercise>)}</div></div>
+            </div>
+
+            <div className='col-2 border'> <h1>how are you </h1></div>
         </div>
     );
 };
